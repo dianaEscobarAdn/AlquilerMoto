5883494245d788d917faf0ece53d8515b6a2b983
dianaEscobarAdn <diana.escobar@ceiba.com.co>
2021-06-15 22:20:03 -0500

    Se hacen correcciones de codigo Smells

--- microservicio/dominio/src/main/java/com/ceiba/alquiler/servicio/persona/ServicioCrearPersona.java
+++ microservicio/dominio/src/main/java/com/ceiba/alquiler/servicio/persona/ServicioCrearPersona.java
@@ -1,10 +1,13 @@
 package com.ceiba.alquiler.servicio.persona;
 
+import com.ceiba.alquiler.dominio.ValidadorArgumento;
 import com.ceiba.alquiler.modelo.dto.DtoRespuestaPersona;
 import com.ceiba.alquiler.modelo.entidad.Persona;
 import com.ceiba.alquiler.puerto.repositorio.RepositorioPersona;
 
 public class ServicioCrearPersona {
+
+    public static final String ERROR_NO_ENCONTRO_LA_PERSONA_CREADA = "Error: no encontro la persona creada";
 
     private final RepositorioPersona repositorioPersona;
 
@@ -15,18 +18,18 @@ public class ServicioCrearPersona {
     public DtoRespuestaPersona ejecutar(Persona persona) {
         Integer idPersona = this.repositorioPersona.crear(persona);
         Persona personaCreada = this.repositorioPersona.buscarPersonaPorId(idPersona);
-        DtoRespuestaPersona dtoRespuestaPersona = convertirADtoRespuesta(personaCreada);
-        return dtoRespuestaPersona;
+        ValidadorArgumento.validarObligatorio(personaCreada, ERROR_NO_ENCONTRO_LA_PERSONA_CREADA);
+        return convertirADtoRespuesta(personaCreada);
     }
 
-    public DtoRespuestaPersona convertirADtoRespuesta (Persona personaCreada){
-        return personaCreada != null ? new DtoRespuestaPersona(
+    public DtoRespuestaPersona convertirADtoRespuesta(Persona personaCreada) {
+        return new DtoRespuestaPersona(
                 personaCreada.getIdPersona(),
                 personaCreada.getCedula(),
                 personaCreada.getNombre(),
                 personaCreada.getApellido(),
                 personaCreada.getTelefono(),
                 personaCreada.getDireccion()
-        ) : null;
+        );
     }
 }
