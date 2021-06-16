aea713ad49036c0cfd5cafd2af0cb2df8af2e9e5
dianaEscobarAdn <diana.escobar@ceiba.com.co>
2021-06-15 22:44:23 -0500

    Se hacen correcciones de codigo Smells

--- microservicio/dominio/src/main/java/com/ceiba/alquiler/servicio/producto/ServicioCrearProducto.java
+++ microservicio/dominio/src/main/java/com/ceiba/alquiler/servicio/producto/ServicioCrearProducto.java
@@ -1,5 +1,6 @@
 package com.ceiba.alquiler.servicio.producto;
 
+import com.ceiba.alquiler.dominio.ValidadorArgumento;
 import com.ceiba.alquiler.modelo.dto.DtoRespuestaProducto;
 import com.ceiba.alquiler.modelo.entidad.Producto;
 import com.ceiba.alquiler.puerto.repositorio.RepositorioProducto;
@@ -9,6 +10,8 @@ public class ServicioCrearProducto {
 
     private final RepositorioProducto repositorioProducto;
 
+    public static final String ERROR_NO_ENCONTRO_EL_PRODUCTO_CREADO = "Error: no encontro el producto creado";
+
     public ServicioCrearProducto(RepositorioProducto repositorioProducto) {
         this.repositorioProducto = repositorioProducto;
     }
@@ -16,16 +19,17 @@ public class ServicioCrearProducto {
     public DtoRespuestaProducto ejecutar(Producto producto) {
        Integer idProducto = this.repositorioProducto.crear(producto);
        Producto productoCreado = this.repositorioProducto.buscarProductoPorId(idProducto);
+       ValidadorArgumento.validarObligatorio(productoCreado, ERROR_NO_ENCONTRO_EL_PRODUCTO_CREADO);
        return convertirADtoRespuesta(productoCreado);
     }
 
     public DtoRespuestaProducto convertirADtoRespuesta (Producto productoCreado){
-        return productoCreado != null ? new DtoRespuestaProducto(
+        return new DtoRespuestaProducto(
                 productoCreado.getIdProducto(),
                 productoCreado.getCodigoProducto(),
                 productoCreado.getDescripcionProducto(),
                 productoCreado.getUnidadesDisponibles(),
                 productoCreado.getUnidadesComprometidas()
-        ) : null;
+        );
     }
 }
